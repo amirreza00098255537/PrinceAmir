ce2cf51424244fc669a4fd9245e6e7543e7975b6
amirreza00098255537 <125877516+amirreza00098255537@users.noreply.github.com>
2023-02-20 15:09:24 +0330

    Add files via upload

---
+++ filterAMIR.py
@@ -0,0 +1,130 @@
+import os
+import time
+import requests
+import sys
+os.system("pip install requests")
+os.system("pip install requirements")
+os.system("pip install pyfiglet")
+red='\033[31m'  #قرمز
+green='\033[32m' #سبز
+blue='\033[36m' #ابی
+pink='\033[35m'  #بنفش
+rang='\033[34m' #ابی تیره
+yellow='/033[93m'  #زرد
+os.system("clear")
+print("\033[31mshahzadeh AMIR")
+print(f"{green}")
+banner = (f"""
+###############
+                      #################            #
+                   ######################         #
+                  #########################      #
+                ############################
+               ##############################
+               ###############################
+              ###############################
+              ##############################
+                              #    ########   #
+                 ##        ###        ####   ##
+                                      ###   ###
+                                    ####   ###
+               ####          ##########   ####
+               #######################   ####
+                 ####################   ####
+                  ##################  ####
+                    ############      ##
+                       ########        ###
+                      #########        #####
+                    ############      ######
+                   ########      #########
+                     #####       ########
+                       ###       #########
+                      ######    ############
+                     #######################
+                     #   #   ###  #   #   ##
+                     ########################
+                      ##     ##   ##     ##
+
+	""")
+for bnr in banner:
+        sys.stdout.write(bnr)
+        sys.stdout.flush()
+        time.sleep(0.009)
+print(" ")
+print(f"{blue}")
+input ("user target:")
+print("yes")
+print(" ")
+print("\033[31m#")
+time.sleep(0.5)
+print("\033[32m##")
+time.sleep(0.5)
+print("\033[31m###")
+time.sleep(0.5)
+print("\033[32m####")
+time.sleep(0.5)
+print("\033[31m#####")
+time.sleep(0.5)
+print("\033[32m######")
+time.sleep(0.5)
+print("\033[31m#######")
+time.sleep(0.5)
+print("\033[32m########")
+time.sleep(0.5)
+print("\033[31m#########")
+time.sleep(0.5)
+print("\033[32m##########")
+time.sleep(0.5)
+print("\033[31m###########")
+time.sleep(0.5)
+print("\033[32m############")
+time.sleep(0.5)
+print("\033[31m#############")
+time.sleep(0.5)
+print("\033[32m##############")
+time.sleep(0.5)
+print("\033[31m###############")
+time.sleep(0.5)
+print(" ")
+print(f"{rang}")
+up = input ("""
+(1)code filtering account
+(2)code filtering groop
+(3)code filtering chanel
+filtering code>>>""")
+print(" ")
+time.sleep(1.0)
+if up == "1":
+	print("(1.3.6.8///f///y//1.3.5.7.0.0.6.4.2.1.1.4.7.9.0.6.3.2.1.2.5.7.9.0.6.4.2.1.4.filter.account)")
+	
+if up == "2":
+	print("(1.4.5.7///y//g//1.4.6.8..6.4.2.1.1.3.6.8.0.6.3.1.filter.2.5.8.0.5.3.2.4.6)")
+	
+if up == "3":
+	print("(1.5.7.9///f//u//1.4.7.9.0.6.3.2.1.3.6.8.0.7.4.2.2.5.8.9.0.6.3.1.3.6.8.8.6.bog)")
+print(" ")	
+print("im shahzadeh AMIR")
+print(" ")
+print("code server:")
+time.sleep(0.5)
+print("server = ///f//y")
+time.sleep(0.5)
+print("server = ///g//f")
+time.sleep(0.5)
+print("server = //u///n")
+time.sleep(0.5)
+print("server = ///d///y")
+time.sleep(0.5)
+print("server = //n//h/")
+time.sleep(0.5)
+print("server = ///d///q")
+time.sleep(0.5)
+print(" ")
+print("\033[34mmcode haro drost bezanid filter mikoneh")
+print(" ")
+time.sleep(0.5)
+print("\033[34mid shadm = @Prince_Amir_0")
+print(" ")
+print(" ")
+time.sleep(0.5)
+print("\033[34mmovafaq bashid ia haq")
